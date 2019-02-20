a17bc4caa05880e19ebfce38a6841b71dbf0cc09
Jose <shine-thadathil.jose@dxc.com>
2019-02-20 19:42:18 +0530

    InitialCommit

---
+++ RmgErpApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RmgErpApp.API.Models;
+
+namespace RmgErpApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
